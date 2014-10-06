b26f494e21e32b29dfce4e13cbb0bc08013f1f3f
David Blanck <dblanck@wowway.com>
2014-10-05 20:02:52 -0400

    first commit

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+    
+//        let sizeFromTextField = mensShoeSizeTextField.text          //get entered string
+//        let numberFromTextField = sizeFromTextField.toInt()         //get an optional
+//        var integerFromTextField = numberFromTextField!             //unwrap optional
+        
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false                   //unhide label
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+        
+        
+//        //set conversion costant
+//        integerFromTextField += conversionConstant                  // convert
+//        mensConvertedShoeSizeLabel.hidden = false                   //unhide label
+//        let stringWithUpdatedShoeSize = "\(integerFromTextField)"   //convert to string
+//        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize //update label
+    
+    }
 
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton) {
+    
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+
+    
+    }
 }
 
